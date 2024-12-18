1d8e2aa0deb4305a2e9e12c742fb90e8fb51348a
T7890u <161718235+T7890u@users.noreply.github.com>
2024-12-18 18:50:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "DYwHzJaD#MB3rJZuMn0vwha7bky83MKFCTKRh8qp5pu86FfsSJcU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
